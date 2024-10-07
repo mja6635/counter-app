08488df7f214a81077cb236b1ce2fe00f4980361
mja6635 <mja6635@psu.edu>
2024-10-06 21:23:55 -0400

    Update to everything

--- src/counter-app.js
+++ src/counter-app.js
@@ -1,56 +1,84 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 
-export class counterApp extends DDDSuper(LitElement) {
-
+export class CounterApp extends DDDSuper(LitElement) {
   static get tag() {
     return "counter-app";
   }
 
   constructor() {
     super();
-    this.title = "";
+    this.title = "Counter App";
+    this.count = 0;
+    this.min = 0;
+    this.max = 25;
   }
 
   static get properties() {
     return {
       title: { type: String },
+      count: { type: Number },
+      min: { type: Number },
+      max: { type: Number },
     };
   }
 
   static get styles() {
-    return [super.styles,
-    css`
-      :host {
-        display: block;
-        color: var(--ddd-theme-primary);
-        background-color: var(--ddd-theme-accent);
-        font-family: var(--ddd-font-navigation);
-        font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
-      }
-      .wrapper {
-        margin: var(--ddd-spacing-2);
-        padding: var(--ddd-spacing-4);
-      }
-      div {
-        padding: 0;
-        margin: 0;
-      }
-    `];
+    return [
+      ...(super.styles || []), 
+      css`
+        :host {
+          display: block;
+          color: var(--ddd-theme-default-nittanyNavy);
+          background-color: var(--ddd-theme-default-potential50);
+          font-family: var(--ddd-font-navigation);
+          font-size: var(--counter-app-font-size, var(--ddd-font-size-s));
+        }
+        .wrapper {
+          margin: var(--ddd-spacing-2);
+          padding: var(--ddd-spacing-4);
+          text-align: center;
+        }
+        .count {
+          font-size: 2rem;
+          margin-bottom: var(--ddd-spacing-2);
+        }
+        button {
+          margin: var(--ddd-spacing-1);
+          padding: var(--ddd-spacing-2);
+          font-size: 1rem;
+          cursor: pointer;
+        }
+        button:disabled {
+          background-color: gray;
+          cursor: not-allowed;
+        }
+      `
+    ];
   }
 
-  increment(e) {
-    this.count++;
+  increment() {
+    if (this.count < this.max) {
+      this.count++;
+    }
+  }
+
+  decrement() {
+    if (this.count > this.min) {
+      this.count--;
+    }
   }
 
   render() {
     return html`
-<div class="wrapper">
-  <div>${this.count}</div>
-  <button>:D</button><button @click="${this.increment}">.</button>
-  <div>${this.title}</div>
-  <slot></slot>
-</div>`;
+      <div class="wrapper">
+        <div class="count">${this.count}</div>
+        <button @click="${this.increment}">Increase</button>
+        <button @click="${this.decrement}">Decrease</button>
+        <div>${this.title}</div>
+        <slot></slot>
+      </div>
+    `;
   }
 
   /**
@@ -62,4 +90,5 @@ export class counterApp extends DDDSuper(LitElement) {
   }
 }
 
-globalThis.customElements.define(counterApp.tag, counterApp);
+
+globalThis.customElements.define(CounterApp.tag, CounterApp);
